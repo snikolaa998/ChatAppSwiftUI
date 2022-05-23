5f09e7fd33a232649d0c6325e1750c5d89b7e2f6
Nikola Savic <snikolaa998@gmail.com>
2022-05-23 22:31:39 +0200

    Initial Commit

---
+++ SwiftUIChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIChat
+//
+//  Created by Nikola Savic on 23.5.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatApp.swift
+//  SwiftUIChat
+//
+//  Created by Nikola Savic on 23.5.22..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
